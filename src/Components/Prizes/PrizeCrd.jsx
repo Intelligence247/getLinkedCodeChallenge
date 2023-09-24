145e99d8845e674ba593561688dcab63ac4e6f05
Intelligence247 <ua67527056@gmail.com>
2023-09-24 07:58:57 +0100

    fixes

--- src/Components/Prizes/PrizeCrd.jsx
+++ src/Components/Prizes/PrizeCrd.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const PrizeCrd = (props) => {
   return (
-    <div className={` lg:w-[13.25rem] w-[32%] lg:h-[16.5rem] h-[8rem] border-2 rounded-lg relative ${props.style} flex flex-col justify-end items-center p-4`}>
-        <img src={props.img} alt="" className={`absolute -top-[30%] ${props.imgSize}`}/>
+    <div className={`lg:w-[13.25rem] w-[32%] lg:h-[16.5rem] h-[9rem] border-2 rounded-lg relative ${props.style} flex flex-col justify-end items-center lg:p-4 p-2`}>
+        <img src={props.img} alt="" className={`absolute lg:-top-[30%] -top-[35%] ${props.imgSize}`}/>
       <h1 className='lg:text-[2rem] text-[12px]'>{props.num}</h1>
       <p className='lg:text-3xl text-[12px]  font-bold'>{props.position}</p>
       <p className='lg:text-2xl text-[12px]'>{'Runner'}</p>
